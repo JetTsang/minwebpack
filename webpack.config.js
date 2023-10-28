b251bf78894e5b85b2915ff2dd8f395f85a90638
JetTsang <1109126828@qq.com>
2023-10-28 21:47:57 +0800

    init webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     filename: "bundle.js", // 输出文件名
     path: path.resolve(__dirname, "dist"), // 输出目录
   },
+  devtool: "source-map",
 //   module: {
 //     rules: [
 //       {
